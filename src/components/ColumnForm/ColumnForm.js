3d2ca1f8e6dba1e655974c0cbfb1d6bdc92b31d3
Anna Pratnicka <pratnicka.ann@gmail.com>
2023-01-22 13:37:51 +0100

    Zmiana formularzy i podlaczenie do stora

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,34 +1,38 @@
-import styles from './ColumnForm.module.scss';
-import { useState } from 'react';
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
+import styles from "./ColumnForm.module.scss";
+import { useState } from "react";
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+import { useDispatch } from "react-redux";
 
-const ColumnForm = props => {
-    const [value, setValue] = useState('');
-    
-    const handleSubmit = e => {
-      e.preventDefault();
-      props.action({ title: title, icon: icon });
-      setTitle('');
-      setIcon('');
-    };
+const ColumnForm = (props) => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("");
 
-    const [title, setTitle] = useState('');
-    const [icon, setIcon] = useState('');
- 
-   return (
-      <form className={styles.columnForm} onSubmit={handleSubmit}>
-            <div>
-                  <span>Title:</span>
-                  <TextInput value={title} onChange={e => setTitle(e.target.value)} />
-            </div>
-            <div>
-                  <span>Icon:</span>
-                  <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
-            </div>
-            <div><Button>Add column</Button></div>
-      </form>
-	);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_COLUMN", payload: { title, icon } });
+    setTitle("");
+    setIcon("");
+  };
+
+  const [title, setTitle] = useState("");
+  const [icon, setIcon] = useState("");
+
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      <div>
+        <span>Title:</span>
+        <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      <div>
+        <span>Icon:</span>
+        <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
+      </div>
+      <div>
+        <Button>Add column</Button>
+      </div>
+    </form>
+  );
 };
 
 export default ColumnForm;
