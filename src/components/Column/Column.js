a08fd25f5206208fe8b62d27299bf37022fad849
Anna Pratnicka <pratnicka.ann@gmail.com>
2023-01-22 13:10:11 +0100

    Redux dodany

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,22 +1,26 @@
-import styles from './Column.module.scss';
-import Card from '../Card/Card';
-import CardForm from '../CardForm/CardForm';
+import styles from "./Column.module.scss";
+import Card from "../Card/Card";
+import CardForm from "../CardForm/CardForm";
+import { useSelector } from "react-redux";
 
-const Column = props => {
-    
-    return (
-        <article className={styles.column}>
-            <h2 className={styles.title}>
-              <span className={styles.icon + ' fa fa-' + props.icon} />
-              {props.title}
-            </h2>
-            <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
-            </ul>
-            <CardForm columnId={props.id} action={props.action} />
-        </article>
-    );
+const Column = (props) => {
+  const cards = useSelector((state) =>
+    state.cards.filter((card) => card.columnId === props.id)
+  );
+  return (
+    <article className={styles.column}>
+      <h2 className={styles.title}>
+        <span className={styles.icon + " fa fa-" + props.icon} />
+        {props.title}
+      </h2>
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card key={card.id} title={card.title} />
+        ))}
+      </ul>
+      <CardForm columnId={props.id} action={props.action} />
+    </article>
+  );
 };
 
 export default Column;
-
